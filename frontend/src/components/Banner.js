834aa99f618afe63de45af92e404d4561cb24ff4
Kavy26 <kavyu22@iitk.ac.in>
2024-10-26 18:51:15 +0530

    Updated Frontend (Added Products Link)

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
       <div className="banner-content">
         <h1>Your One-Stop Online Shop</h1>
         <p>Explore exclusive deals on top-quality products tailored just for you.</p>
-        <button className="shop-now-btn"><a href="/products"> Shop Now</a></button>
+        <button className="shop-now-btn"><a href="/Products"> Shop Now</a></button>
       </div>
     </div>
   );
